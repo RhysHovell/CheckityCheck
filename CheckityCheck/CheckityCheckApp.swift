9bafb49aee125a128d1c6ad5a0d86c72f0cb014c
rhyshovell <rhyshovell@gmail.com>
2023-10-11 17:24:45 +0100

    Initial Commit

---
+++ CheckityCheck/CheckityCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckityCheckApp.swift
+//  CheckityCheck
+//
+//  Created by Rhys Hovell on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckityCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
